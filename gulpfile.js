171b4d2c2e57e531b5a2f9b7ec71dfe9f722043b
Sergey Sovgut <sergeypba@gmail.com>
2018-12-14 19:30:56 +0200

    Uplated to gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,71 +1,51 @@
 /**
  * Sass configuration
  */
-const gulp    = require('gulp'),
-      sass    = require('gulp-sass'),
-      util    = require('gulp-util'),
-      notify  = require('gulp-notify'),
-      plumber = require('gulp-plumber');
-
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
 /**
  * Source scss files
  */
-// Main file. Use * .scss to compile everything into different files and not into one 
-const sassPath  = 'scss/main.scss';
+// Main file. Use * .scss to compile everything into different files and not into one
+const sassPath = 'scss/main.scss';
 // Watching all the files in the folder scss/
 const sassWatch = 'scss/**/*.scss';
 
 /**
  * Output css files
  */
-const cssPath   = 'css/';
+const cssPath = 'css/';
 
 /**
  * Creating build the task
  */
-gulp.task('sass', function() {
-
+gulp.task('sass', done => {
   /**
    * Source path
    * 'scss/*.scss' or 'scss/main.scss'
    */
-  gulp.src(sassPath)
+  gulp
+    .src(sassPath)
 
-      /**
-       * If the error, write a message to the console and continues to work
-       */
-      .pipe(plumber({ errorHandler: function(error) {
+    /**
+     * Compiling source and minify output
+     */
+    .pipe(sass({ outputStyle: 'compressed' }))
 
-        /**
-         * Using system notify center for showing error message
-         */
-        notify.onError({
-          title: 'Gulp error in ' + error.plugin,
-          message: error.message.toString()
-        })(error);
-
-
-        /**
-         * Using system sounds
-         */
-        util.beep();
-      }}))
-
-      /**
-       * Compiling source and minify output
-       */
-      .pipe(sass({outputStyle: 'compressed'}))
-
-      /**
-       * Destination path
-       */
-      .pipe(gulp.dest(cssPath));
+    /**
+     * Destination path
+     */
+    .pipe(gulp.dest(cssPath));
+  done();
 });
 
 /**
  * Creating watch the task
  */
- gulp.task('watch', ['sass'], function() {
-  gulp.watch(sassWatch, ['sass']);
- });
+gulp.task(
+  'watch',
+  gulp.series('sass', () => {
+    gulp.watch(sassWatch, gulp.series('sass'));
+  }),
+);
